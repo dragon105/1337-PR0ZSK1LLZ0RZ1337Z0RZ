80e830ab00a34a533ada284ef491409fcfd81624
dragon105 <xavier.rivera@nexgenacademy.com>
2014-12-11 15:19:21 -0700

    Screw summaries
    Descriptions are for n00bz0rdz

---
+++ Arcade Space Fighter 1/src/arcade/space/fighter/pkg1/Enemies.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arcade.space.fighter.pkg1;
+
+/**
+ *
+ * @author Cicada
+ */
+public class Enemies {
+    int HP; // health
+    float xPos; // position on x axis
+    float yPos; // position on y axis
+    float[] hitbox = new float[2]; // size of hitbox relative to object
+    float heading; // heading from 0 to 360
+    float speed; // speed per tick
+    boolean dead; // object does nothing if dead
+    String enemyType; // type of enemy
+    
+    void create(){ // called on creation. Gives starting values based on type.
+    }
+    
+    void render(){ // renders enemy on canvas
+        
+    }
+    
+    void update(){ // updates values
+        
+    }
+}
---
+++ Arcade Space Fighter 1/src/arcade/space/fighter/pkg1/Misc.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arcade.space.fighter.pkg1;
+
+/**
+ *
+ * @author Cicada
+ */
+public class Misc {
+    float xPos; // position on x axis
+    float yPos; // position on y axis
+    float[] hitbox = new float[2]; // size of hitbox relative to object
+    float heading; // heading from 0 to 360
+    float speed; // speed per tick
+    boolean dead; // object does nothing if dead
+    String miscType; // type of misc object
+}
